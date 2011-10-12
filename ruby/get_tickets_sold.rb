07715532eac2a7966962fe6d5a9228f1da5787d0
Tiem Song <tiem@songfamily.org>
2011-10-11 22:03:33 -0700

    script to find number of tickets sold on eventbrite's home page

---
+++ ruby/get_tickets_sold.rb
@@ -0,0 +1,16 @@
+#!/bin/env ruby
+
+require 'net/http'
+require 'uri'
+
+total = ""
+
+res = Net::HTTP.get('www.eventbrite.com', '/')
+res.split("\n").each do |r|
+  break if r.match(/Tickets sold/) # don't process entire HTTP return
+  if r.match(/ticker_(\d)/) # see if line contains the ticker class
+    total += $1 # concat to end of total string
+  end
+end
+
+puts total
