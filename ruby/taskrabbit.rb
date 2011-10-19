190fe32635ed0adc3ed090df0b715d5e66c5a386
Tiem Song <tiem@songfamily.org>
2011-10-18 18:33:49 -0700

    update taskrabbit script

--- ruby/taskrabbit.rb
+++ ruby/taskrabbit.rb
@@ -1,9 +1,16 @@
 #!/bin/env ruby
 
-require 'net/http'
+require 'hpricot'
+require 'open-uri'
 
-res = Net::HTTP.get('www.taskrabbit.com', '/')
-res.split("\n").each do |r|
-  puts r
-end
+# grab HTML and parse with Hpricot
+doc = open("http://www.taskrabbit.com"){|f| Hpricot(f)}
 
+# get list of tasks
+tasks = []
+tasks_html = doc.search("div.clear/ul/li/div/p/a")
+tasks_html.each {|t| tasks << t.inner_html}
+
+# output list of tasks
+puts "Tasks on taskrabbit.com landing page:"
+tasks.each {|task| puts "- #{task}"}
